6e1851ef23d2bbf6d0c452ab306c2b261678e704
eldorado0786 <mdatifraza926@gmail.com>
2024-07-17 18:47:26 +0530

    added the solution for the question

--- Co-hort/class-1/main.js
+++ Co-hort/class-1/main.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+// Q1 Write a program to greet a person given their first and last name
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+
+greet("MD ATIF RAZA");
+
+
+// Q2 Write a program that greets a person based on their gender .(If-else)
+function greet(name, gender) {
+    if (gender == "male") {
+        console.log(`Hello Mr. ${name}`);
+    }
+    else if (gender == "female") {
+        console.log(`Hello Mrs. ${name}`);
+    }
+    else {
+        console.log(`Hello ${name}`);
+    }
+}
+
+greet("MD ATIF RAZA", "male")
+
+greet("Mrs RAZA", "female")
+
+// Q3 
+
+
