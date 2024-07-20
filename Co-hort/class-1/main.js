3faff0f3c55cb8d7cb6840222497d22be2adac59
eldorado0786 <mdatifraza926@gmail.com>
2024-07-20 23:31:06 +0530

    improved readability

--- Co-hort/class-1/main.js
+++ Co-hort/class-1/main.js
@@ -7,7 +7,6 @@ function greet(name) {
 
 greet("MD ATIF RAZA");
 
-
 // Q2 Write a program that greets a person based on their gender .(If-else)
 function greet(name, gender) {
     if (gender == "male") {
